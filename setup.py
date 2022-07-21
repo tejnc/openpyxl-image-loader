e86ba8516afb91d0856ff26aec9a8fb03b2e2cb1
Tej Narayan Chaudhary <91244907+tejnc@users.noreply.github.com>
2022-07-21 16:32:47 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ README = (BASE_DIR / 'README.md').read_text()
 
 setup(
     name='openpyxl-image-loader',
-    version='1.0.5',
+    version='0.1.0',
     description='Openpyxl wrapper that gets images from cells',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/mauricio-chavez/openpyxl-image-loader',
-    author='Mauricio Chávez Olea',
-    author_email='mauriciochavez@ciencias.unam.mx',
+    author='Tej Narayan Chaudhary',
+    author_email='tejnarayan15.tnc@gmail.com',
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
@@ -21,5 +21,5 @@ setup(
     ],
     packages=['openpyxl_image_loader'],
     include_package_data=True,
-    install_requires=['Pillow', ],
+    install_requires=['Pillow','openpyxl',],
 )
