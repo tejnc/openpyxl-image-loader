6359c2e64e64dd77526f7aaadebd9a09d623f414
tejnc <tejchaudhary@cagtu.com>
2022-07-24 12:23:36 +0545

    package name changed

---
+++ openpyxl_image_loader_fork/__init__.py
@@ -0,0 +1,7 @@
+"""openpyxl_image_loader-fork - Openpyxl wrapper that gets images from cells"""
+
+__version__ = '0.1.0'
+__author__ = 'Tej Narayan Chaudhary <tejnarayan15.tnc@gmail.com>'
+__all__ = ['SheetImageLoader']
+
+from openpyxl_image_loader_fork.sheet_image_loader import SheetImageLoader  # NOQA
